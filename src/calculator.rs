251a0166a88c7dae5104df4032c9d9fc35278724
Joshua Flowers <flowers.joshuac@gmail.com>
2024-10-31 21:30:24 -0500

    Project rewrite
    Moved core calculator components of `application.rs` to `calculator.rs`,
    removing components related to input parsing and calculator rendering
    (these should have been separate from the start)

---
+++ src/calculator.rs
@@ -0,0 +1,115 @@
+use std::collections::HashMap;
+use std::f64::consts::PI;
+
+#[derive(Debug)]
+pub enum Command {
+	BinOp(BinOp),
+	UnOp(UnOp),
+	Push(Option<f64>),
+	Sto(char),
+	Del(char),
+	Rcl(char),
+}
+#[derive(Debug)]
+pub enum BinOp {
+	Add,
+	Sub,
+	Mul,
+	Div,
+	Swp,
+	Pow,
+	Root,
+	Exp, // 10^x
+	IntDiv,
+	Mod,
+}
+#[derive(Debug)]
+pub enum UnOp {
+	Neg,
+	Sqrt,
+	Sqr,
+	Sin,
+	Cos,
+	Tan,
+	Asin,
+	Acos,
+	Atan,
+	Rad,
+	Deg,
+	Pop,
+}
+
+// TODO: Make this configurable at runtime
+const STACK_SIZE: usize = 12;
+pub struct Calculator {
+	nums: [f64; STACK_SIZE],
+	memory: HashMap<char, f64>,
+}
+impl Calculator {
+	pub fn new() -> Calculator {
+		Calculator {
+			nums: [0.0; STACK_SIZE],
+			memory: HashMap::new(),
+		}
+	}
+
+	pub fn process_command(&mut self, cmd: Command) {
+		match cmd {
+			// Stack commands
+			Command::BinOp(op) => {
+				match op {
+					BinOp::Add => self.rotate_out(self.nums[1] + self.nums[0]),
+					BinOp::Sub => self.rotate_out(self.nums[1] - self.nums[0]),
+					BinOp::Mul => self.rotate_out(self.nums[1] * self.nums[0]),
+					BinOp::Div => self.rotate_out(self.nums[1] / self.nums[0]),
+					BinOp::IntDiv => self.rotate_out((self.nums[1] / self.nums[0]) % 1.0), // TODO: Test this!
+					BinOp::Swp => {
+						let tmp = self.nums[1];
+						self.nums[1] = self.nums[0];
+						self.nums[0] = tmp;
+					},
+					BinOp::Pow => self.rotate_out(self.nums[1].powf(self.nums[0])),
+					BinOp::Root => self.rotate_out(self.nums[1].powf(1.0 / self.nums[0])),
+					BinOp::Exp => self.rotate_out(self.nums[1] * (10.0f64).powf(self.nums[0])),
+					BinOp::Mod => self.rotate_out(self.nums[1] % self.nums[0]),
+				}
+			},
+			Command::UnOp(op) => {
+				match op {
+					UnOp::Neg => self.nums[0] = -self.nums[0],
+					UnOp::Sqrt => self.nums[0] = self.nums[0].sqrt(),
+					UnOp::Sqr => self.nums[0] = self.nums[0].powf(2.0),
+					UnOp::Sin => self.nums[0] = self.nums[0].sin(),
+					UnOp::Cos => self.nums[0] = self.nums[0].cos(),
+					UnOp::Tan => self.nums[0] = self.nums[0].tan(),
+					UnOp::Asin => self.nums[0] = self.nums[0].asin(),
+					UnOp::Acos => self.nums[0] = self.nums[0].acos(),
+					UnOp::Atan => self.nums[0] = self.nums[0].atan(),
+					UnOp::Rad => self.nums[0] = (self.nums[0] / 360.0) * (2.0 * PI),
+					UnOp::Deg => self.nums[0] = (self.nums[0] * 360.0) / (2.0 * PI),
+					UnOp::Pop => self.rotate_out(self.nums[1]),
+				}
+			},
+			Command::Push(val) => self.rotate_in(val.unwrap_or(self.nums[0])),
+			// Memory commands
+			Command::Sto(key) => { self.memory.insert(key, self.nums[0]); },
+			Command::Del(key) => { self.memory.remove(&key); },
+			Command::Rcl(key) => if let Some(v) = self.memory.get(&key).copied() { self.rotate_in(v); },
+		}
+	}
+
+	fn rotate_in(&mut self, num: f64) {
+		for i in (0..STACK_SIZE-1).rev() {
+			self.nums[i+1] = self.nums[i];
+		}
+		self.nums[0] = num;
+	}
+	fn rotate_out(&mut self, num: f64) {
+		// Duplication of the first value on the stack is intentional
+		// This is emulating the behavior of an RPN calculator I've used before
+		for i in 1..STACK_SIZE {
+			self.nums[i-1] = self.nums[i];
+		}
+		self.nums[0] = num;
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,5 @@
-mod application;
-mod command;
-mod tui_windows;
+mod calculator;
 
-use application::{Calculator, Memory, Response};
-use tui_windows::*;
-
-use crossterm::execute;
-use crossterm::cursor::{position, Hide, Show};
-use crossterm::event::read;
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode, size, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen};
-
-use std::cell::RefCell;
-
-fn main() -> Result<(), std::io::Error> {
-	enable_raw_mode()?;
-
-	let memory = RefCell::new(Memory::new());
-	let calc = RefCell::new(Calculator::new(&memory));
-	let window_calc = Window::new(&calc, WindowConfig { rel_size: 1.0, wrapping: true });
-	let window_mem = Window::new(&memory, WindowConfig { rel_size: 1.1, wrapping: true });
-	let app = Container::new(vec!(TileType::Window(window_calc), TileType::Window(window_mem)), false, 1.0);
-
-	let mut out = std::io::stdout();
-	execute!(out, EnterAlternateScreen, Clear(ClearType::All), Hide)?;
-	loop {
-		let (cols, rows) = size()?;
-		let (cur_col, cur_row) = position()?;
-
-		app.draw(&mut out, (cur_row, cur_col), (0, 0), (rows, cols))?;
-
-		let mut calc_borrow = calc.borrow_mut();
-		let code = calc_borrow.process_event(read().unwrap())?;
-		match code {
-			Response::NoAction => (),
-			Response::Exit => break,
-		}
-	}
-
-	execute!(std::io::stdout(), LeaveAlternateScreen, Show)?;
-	disable_raw_mode()?;
-	Ok(())
+fn main() {
+	println!("Hello, World!");
 }
