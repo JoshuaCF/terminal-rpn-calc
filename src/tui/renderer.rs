d522d07a049c57b9dc9a8642dc018eb3af5bd9e9
Joshua Flowers <flowers.joshuac@gmail.com>
2026-07-24 20:27:20 -0500

    Improved layout distribution when stack is not on the left

--- src/tui/renderer.rs
+++ src/tui/renderer.rs
@@ -100,7 +100,7 @@ pub enum MemoryAlignment {
 }
 
 /// Stores all of the configuration values for the renderer.
-#[derive(Default, Serialize, Deserialize)]
+#[derive(Clone, Copy, Default, Serialize, Deserialize)]
 pub struct RendererConfig {
 	pub colors: Colors,
 	pub stack_alignment: StackAlignment,
@@ -108,100 +108,110 @@ pub struct RendererConfig {
 	pub memory_location: MemoryLocation,
 }
 
-impl TUI {
-	/// Converts an [`f64`] into [`Span`]s representing the number with the styling provided in the
-	/// TUI's configuration. This could then be used to insert into a [`Line`], or be further
-	/// manipulated. Each contiguous sequence of digits will be one span, and the decimal and
-	/// exponent separators will have their own spans dedicated to those characters.
-	fn style_number<'a>(&self, number: f64, width: u16) -> Vec<Span<'a>> {
-		// First try doing a plain format
-		// If too long, then try exponential with precision of WIDTH-3 (decimal separator, e, and
-		// digit)
-		// Continue reducing precision until it fits
-		// Maybe there's a smarter way, but this is plenty good enough
-		let mut num_string = format!("{:>1}", number);
-		// .len() acceptable here since number formatting will only use ASCII which is 1 char to
-		// 1 byte
-		let mut precision = (width - 3) as usize;
-		while num_string.len() > width as usize {
-			num_string = format!("{:.precision$e}", number);
+/// Converts an [`f64`] into [`Span`]s representing the number with the styling provided in the
+/// TUI's configuration. This could then be used to insert into a [`Line`], or be further
+/// manipulated. Each contiguous sequence of digits will be one span, and the decimal and
+/// exponent separators will have their own spans dedicated to those characters.
+fn style_number<'a>(number: f64, width: u16, config: RendererConfig) -> Vec<Span<'a>> {
+	// First try doing a plain format
+	// If too long, then try exponential with precision of WIDTH-3 (decimal separator, e, and
+	// digit)
+	// Continue reducing precision until it fits
+	// Maybe there's a smarter way, but this is plenty good enough
+	let mut num_string = format!("{:>1}", number);
+	// .len() acceptable here since number formatting will only use ASCII which is 1 char to
+	// 1 byte
+	let mut precision = (width - 3) as usize;
+	while num_string.len() > width as usize {
+		num_string = format!("{:.precision$e}", number);
 
-			if precision == 0 {
-				// can't see how this could happen but just in case
-				// TODO: handle this better
-				return vec![Span::from("Render err")];
-			}
-			precision -= 1;
+		if precision == 0 {
+			// can't see how this could happen but just in case
+			// TODO: handle this better
+			return vec![Span::from("Render err")];
 		}
-
-		// Style the parts of the number
-		// numbers[decimal numbers[exponent numbers]]
-		let mut cur_spans = vec![];
-
-		let exponent_idx = num_string.find('e');
-		let decimal_idx = num_string.find('.');
-		let mut cur_idx = 0;
-
-		if let Some(to) = decimal_idx {
-			cur_spans.push(
-				String::from(&num_string[cur_idx..to])
-					.set_style(self.render_config.colors.number),
-			);
-			cur_spans.push(
-				String::from(&num_string[to..to + 1])
-					.set_style(self.render_config.colors.decimal_separator),
-			);
-			cur_idx = to + 1; // Again, working with only ASCII so this is okay
-		}
-		if let Some(to) = exponent_idx {
-			cur_spans.push(
-				String::from(&num_string[cur_idx..to])
-					.set_style(self.render_config.colors.number),
-			);
-			cur_spans.push(
-				String::from(&num_string[to..to + 1])
-					.set_style(self.render_config.colors.exponent_separator),
-			);
-			cur_idx = to + 1; // See above
-		}
-		cur_spans.push(
-			String::from(&num_string[cur_idx..]).set_style(self.render_config.colors.number),
-		);
-		cur_spans
+		precision -= 1;
 	}
+
+	// Style the parts of the number
+	// numbers[decimal numbers[exponent numbers]]
+	let mut cur_spans = vec![];
+
+	let exponent_idx = num_string.find('e');
+	let decimal_idx = num_string.find('.');
+	let mut cur_idx = 0;
+
+	if let Some(to) = decimal_idx {
+		cur_spans.push(
+			String::from(&num_string[cur_idx..to])
+				.set_style(config.colors.number),
+		);
+		cur_spans.push(
+			String::from(&num_string[to..to + 1])
+				.set_style(config.colors.decimal_separator),
+		);
+		cur_idx = to + 1; // Again, working with only ASCII so this is okay
+	}
+	if let Some(to) = exponent_idx {
+		cur_spans.push(
+			String::from(&num_string[cur_idx..to])
+				.set_style(config.colors.number),
+		);
+		cur_spans.push(
+			String::from(&num_string[to..to + 1])
+				.set_style(config.colors.exponent_separator),
+		);
+		cur_idx = to + 1; // See above
+	}
+	cur_spans.push(
+		String::from(&num_string[cur_idx..]).set_style(config.colors.number),
+	);
+	cur_spans
+}
+
+struct TUIAreas {
+	command_line: Rect,
+	stack: Rect,
+	memory: Rect,
+}
+fn divide_area(area: Rect, stack_size: usize, config: RendererConfig) -> TUIAreas {
+	// width of 24 is not arbitrary, it permits the full 15 to 17 digits of decimal precision
+	// f64 offers as well as allowing room for decimal separator, exponent separator, and
+	// exponent digits
+	let constraints: Vec<Constraint> = match config.memory_location {
+		MemoryLocation::StackTop => vec![Constraint::Length(stack_size as u16 + 1), Constraint::Percentage(100)],
+		MemoryLocation::StackLeft => vec![Constraint::Length(24), Constraint::Percentage(100)],
+		MemoryLocation::StackBottom => vec![Constraint::Percentage(100), Constraint::Length(stack_size as u16 + 1)],
+		MemoryLocation::StackRight => vec![Constraint::Percentage(100), Constraint::Length(24)],
+	};
+
+	let layout = Layout::new(config.memory_location.into(), constraints).spacing(2);
+
+	let areas: [Rect; 2] = layout.areas(area);
+	let (main_area, memory_area) = match config.memory_location {
+		MemoryLocation::StackLeft | MemoryLocation::StackTop => (areas[0], areas[1]),
+		MemoryLocation::StackRight | MemoryLocation::StackBottom => (areas[1], areas[0]),
+	};
+
+	let main_area_parts: [Rect; 2] = Layout::new(
+		Direction::Vertical,
+		vec![
+			Constraint::Min(stack_size as u16),
+			Constraint::Percentage(100),
+		],
+	)
+	.areas(main_area);
+	let stack_area = main_area_parts[0];
+	let command_area = main_area_parts[1];
+
+	TUIAreas { command_line: command_area, stack: stack_area, memory: memory_area }
 }
 
 impl Widget for &TUI {
 	fn render(self, area: Rect, buf: &mut Buffer) {
-		// width of 24 is not arbitrary, it permits the full 15 to 17 digits of decimal precision
-		// f64 offers as well as allowing room for decimal separator, exponent separator, and
-		// exponent digits
-		let constraints: Vec<Constraint> = match Into::<Direction>::into(self.render_config.memory_location) {
-			Direction::Vertical => vec![Constraint::Length(self.calc.stack.len() as u16 + 1), Constraint::Percentage(100)],
-			Direction::Horizontal => vec![Constraint::Length(24), Constraint::Percentage(100)],
-		};
-
-		let layout = Layout::new(self.render_config.memory_location.into(), constraints).spacing(2);
-
-		let areas: [Rect; 2] = layout.areas(area);
-		let (main_area, memory_area) = match self.render_config.memory_location {
-			MemoryLocation::StackLeft | MemoryLocation::StackTop => (areas[0], areas[1]),
-			MemoryLocation::StackRight | MemoryLocation::StackBottom => (areas[1], areas[0]),
-		};
-
-		let main_area_parts: [Rect; 2] = Layout::new(
-			Direction::Vertical,
-			vec![
-				Constraint::Min(self.calc.stack.len() as u16),
-				Constraint::Percentage(100),
-			],
-		)
-		.areas(main_area);
-		let stack_area = main_area_parts[0];
-		let command_area = main_area_parts[1];
-
+		let TUIAreas { command_line, stack, memory } = divide_area(area, self.calc.stack.len(), self.render_config);
 		// Show error if regions are too small
-		if stack_area.width < 8 || (stack_area.height as usize) < self.calc.stack.len() {
+		if stack.width < 8 || (stack.height as usize) < self.calc.stack.len() {
 			Paragraph::new("Screen too small!")
 				.wrap(Wrap { trim: true })
 				.render(area, buf);
@@ -214,10 +224,10 @@ impl Widget for &TUI {
 		// Format each number per the config and insert it into stack_lines
 		for stack_value in self.calc.stack.iter().rev() {
 			stack_lines.push(Line::from(
-				self.style_number(*stack_value, stack_area.width),
+				style_number(*stack_value, stack.width, self.render_config),
 			).alignment(self.render_config.stack_alignment.into()));
 		}
-		Text::from(stack_lines).render(stack_area, buf);
+		Text::from(stack_lines).render(stack, buf);
 
 		// Memory area rendering
 		let mut memory_lines: Vec<Line> = vec![];
@@ -226,10 +236,10 @@ impl Widget for &TUI {
 			let mut memory_prefix = key.to_string();
 			memory_prefix.push_str(": ");
 
-			let line_width = memory_area.width - memory_prefix.len() as u16;
+			let line_width = memory.width - memory_prefix.len() as u16;
 			cur_line.push_span(memory_prefix.set_style(self.render_config.colors.memory_key));
 
-			let styled_number = self.style_number(*val, line_width);
+			let styled_number = style_number(*val, line_width, self.render_config);
 			if let MemoryAlignment::SplitMiddle = self.render_config.memory_alignment {
 				let number_len = styled_number.iter().fold(0, |acc, e| acc + e.width());
 				cur_line.push_span(" ".repeat(line_width as usize - number_len));
@@ -243,13 +253,13 @@ impl Widget for &TUI {
 			}
 			memory_lines.push(cur_line);
 		}
-		Text::from(memory_lines).render(memory_area, buf);
+		Text::from(memory_lines).render(memory, buf);
 
 		// Command area rendering
 		let mut cur_command = String::new();
 		for cur_char in &self.parser.bfr {
 			cur_command.push(*cur_char);
 		}
-		cur_command.render(command_area, buf);
+		cur_command.render(command_line, buf);
 	}
 }
